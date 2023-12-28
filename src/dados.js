453fa8b6ef0da0c7a43651eabed2251b8ad3f351
Karolayne Silva <96891482+Karolayne-silva@users.noreply.github.com>
2023-12-28 13:19:28 -0300

    passando o json para um array de objetos

---
+++ src/dados.js
@@ -0,0 +1,45 @@
+export const data = [
+  {
+    nome: "Calculadora de idade",
+    imgProjeto: "src/img/projeto 1.png",
+    descricao:
+      "Site de uma calculadora de idade que foi um desafio do front end mentor. Feito em HTML, CSS e JS.",
+    linkProjeto: "https://challenger-age-calculator-app.netlify.app/",
+    linkGithub:
+      "https://github.com/Karolayne-silva/challenge-frontmentor-age-calculator",
+  },
+  {
+    nome: "FAQ",
+    imgProjeto: "src/img/projeto 2.png",
+    descricao: "Desafio do front end mentor utilizando HTML, CSS e JS.",
+    linkProjeto:
+      "https://karolayne-silva.github.io/challenge-frontend-mentor-faq-card/",
+    linkGithub:
+      "https://github.com/Karolayne-silva/challenge-frontend-mentor-faq-card",
+  },
+  {
+    nome: "Sistema de submissão de missão",
+    imgProjeto: "src/img/projeto 3.png",
+    descricao: "tem que explicar direitinho aq",
+    linkProjeto: "https://disporenergia.netlify.app/perfil",
+    linkGithub: "https://github.com/ErikavbSantos/dispor_energia",
+  },
+  {
+    nome: "Portifólio Lobo",
+    imgProjeto: "src/img/projeto 4.png",
+    descricao:
+      "Projeto desenvolvido em um curso da Origamid de um portifólio. Feito em HTML e CSS.",
+    linkProjeto:
+      "https://karolayne-silva.github.io/projeto-portifolio-origamid/",
+    linkGithub:
+      "https://github.com/Karolayne-silva/projeto-portifolio-origamid",
+  },
+  {
+    nome: "Bikcraft",
+    imgProjeto: "src/img/projeto 5.png",
+    descricao:
+      "Projeto desenvolvido no curso da Origamid de um site de vendas de bicicletas. Feito em HTML e CSS.",
+    linkProjeto: "https://karolayne-silva.github.io/site-bikcraft-origamid/",
+    linkGithub: "https://github.com/Karolayne-silva/site-bikcraft-origamid",
+  },
+];
