677c23f31f8fa91c938783998365859320beac5f
Karolayne Silva <96891482+Karolayne-silva@users.noreply.github.com>
2024-02-25 02:04:11 -0300

    mudando menu mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import "../styles.css";
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import imgLetra from "../img/letra K.svg";
 import animacao from "../img/circulo.svg";
 import imgLetraB from "../img/letraKb.svg";
@@ -7,7 +7,7 @@ import animacaoB from "../img/circulo branco.svg";
 import "../styles/header.css";
 import Toggle from "../components/Toggle";
 
-export default function Header({dark, setDark}) {
+export default function Header({ dark, setDark }) {
   const [hasScrolled, setHasScrolled] = useState(false);
 
   useEffect(() => {
@@ -24,35 +24,54 @@ export default function Header({dark, setDark}) {
 
   const [menuOpen, setMenuOpen] = useState(false);
 
-
   return (
     <>
       <header className={hasScrolled ? "header scrolled" : "header"}>
         <div className="header-in">
-
           <div className="imgs-header">
-            <img src={!dark ? imgLetra : imgLetraB} alt="letra k" className="img-letra" />
-            <img src={!dark ? animacao : animacaoB} alt="circulo ondulado" className="animation"/>
+            <img
+              src={!dark ? imgLetra : imgLetraB}
+              alt="letra k"
+              className="img-letra"
+            />
+            <img
+              src={!dark ? animacao : animacaoB}
+              alt="circulo ondulado"
+              className="animation"
+            />
           </div>
 
-          <div  className="menu" onClick={()=>{
-            setMenuOpen(!menuOpen);
-          }}>
-              <span></span>
-              <span></span>
-              <span></span>
+          <div
+            className="menu"
+            onClick={() => {
+              setMenuOpen(!menuOpen);
+            }}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
           </div>
-          
-            <ul className={menuOpen ? "open": ""}>
-              <li><a href="#home">Home</a></li>
-              <li><a href="#sobre">Sobre mim</a></li>
-              <li><a href="#projetos">Projetos</a></li>
-              <li><a href="#contatos">Contatos</a></li>
-              <li><Toggle handleChange={() => setDark(!dark)} isChecked={dark} /></li>
-            </ul>
-          
+          <ul className={menuOpen ? "open" : ""}>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#sobre">Sobre mim</a>
+            </li>
+            <li>
+              <a href="#projetos">Projetos</a>
+            </li>
+            <li>
+              <a href="#contatos">Contatos</a>
+            </li>
+            <li>
+              <Toggle
+                handleChange={() => setDark(dark ? false : true)}
+                isChecked={dark}
+              />
+            </li>
+          </ul>
         </div>
-        
       </header>
     </>
   );
